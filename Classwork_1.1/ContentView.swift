263f888493f3428fe13f28d4d8b9610010cc58bc
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-05-03 23:56:22 +0300

    Initial Commit

---
+++ Classwork_1.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classwork_1.1
+//
+//  Created by Nour El-Sharkawi on 5/3/20.
+//  Copyright © 2020 Nancy El-Sharkawi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
