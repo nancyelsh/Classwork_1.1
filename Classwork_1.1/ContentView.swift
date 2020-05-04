929529cd244cae791e80b5528c92d936e465d57b
Nancy El-Sharkawi <elsharkawinancy@gmail.com>
2020-05-04 07:00:59 +0300

    Classwork 3

--- Classwork_1.1/ContentView.swift
+++ Classwork_1.1/ContentView.swift
@@ -9,25 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var pageNumber = ""
+    @State var thekr = athkar.randomElement()!
+    @State var counter = "0"
     var body: some View {
         ZStack {
             BG()
             VStack {
-                Text("Page \(pageNumber)").font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                TextField("رقم الصفحة", text: $pageNumber).keyboardType(.numberPad)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    .multilineTextAlignment(.trailing)
-                    .frame(height: 90)
-                
+                Text(thekr).modifier(ThekrModifier())
+                Text(counter).modifier(CounterModifier())
             }
+        }
+        .onTapGesture {
+            self.counter = String(Int(self.counter)! + 1)
+        }
+        .onLongPressGesture {
+            self.thekr = athkar.randomElement()!
+            self.counter = "0"
         }
     }
 }
 
+struct ThekrModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .foregroundColor(.white)
+        .multilineTextAlignment(.center)
+        .padding()
+    }
+}
+
+struct CounterModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .foregroundColor(.white)
+        .font(.custom("Al Tarikh", size: 50))
+        .padding(.horizontal, 50)
+        .padding(.vertical, 3)
+        .padding(.bottom, 0)
+        .background(Color.init( #colorLiteral(red: 0.5489003658, green: 0.8530963063, blue: 0.7255270481, alpha: 0.25)))
+         .clipShape(Capsule())
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
